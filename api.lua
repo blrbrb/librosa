a3e3a89e588dc7e85d5a60156a4f5f9574eedfd1
blrbrb <elireynolds23@gmail.com>
2025-05-01 01:50:10 -0400

    05/01

--- api.lua
+++ api.lua
@@ -1,12 +1,15 @@
 -- rosa/api.lua
-Rosa.registered_plants = {}
 
--- generate unique node names per. stage
+-- this is needed because in order to register plants marked as pottable with flowerpot, we need to call flowerpot.register_craft() after mods have loaded.
+-- so we keep track of the registered plant defs with a table attached to the global namespace
+Librosa.registered_plants = {}
+
+-- generate unique node names per. stage, if needed
 local function get_stage_node_name(base_name, stage)
     return base_name .. "_stage_" .. stage
 end
 
--- Register nodes for each growth stage
+-- Register nodes for each growth stage, if needed
 local function register_growth_nodes(name, def)
     for stage, texture in ipairs(def.textures) do
         local node_name = get_stage_node_name(name, stage)
@@ -30,7 +33,7 @@ local function register_growth_nodes(name, def)
     end
 end
 
--- Register nodes for each growth stage
+-- Register nodes for each growth stage. if needed
 local function register_vine_growth_nodes(name, def)
     for stage, texture in ipairs(def.textures) do
         local node_name = get_stage_node_name(name, stage)
@@ -89,6 +92,7 @@ local function register_vine_growth_nodes(name, def)
     end
 end
 
+-- prettify the description of the plant, so that the name of the genus and species appear neatly when hoevered over in the inventory.
 local function format_description(name, def)
     if def.species and not def.genus then
         def.description = def.description ..
@@ -103,36 +107,36 @@ local function format_description(name, def)
         def.description = def.description ..
             "\n" .. core.colorize("#d0ffd0", def.genus) .. "\n" .. core.colorize("#d0ffd0", def.species)
     end
+
+    -- TD: Add qualifiers for temperature, biome, progagation (seed/cutting) etc
 end
 
+
+local function register_pottable_plant(name)
+    if core.global_exists("flowerpot") then
+        flowerpot.register_node(name)
+    else
+        core.debug("(librosa)[WARN] a mod attempted to register " ..
+            name .. "as pottable, but flowerpot is not installed!!")
+    end
+end
 
 local function register_seeds(name, def)
     minetest.register_craftitem(name, {
         description = def.name .. " Seed",
-        inventory_image = def.seed_texture or "blank.png",
-        on_place = function(itemstack, placer, pointed_thing)
-            local pos = pointed_thing.under
-            local node = minetest.get_node(pos)
-
-            -- Check if the node is soil?
-            if minetest.get_item_group(node.name, "soil") ~= 0 then
-                local above = { x = pos.x, y = pos.y + 1, z = pos.z }
-                local above_node = minetest.get_node(above)
-
-                if above_node.name == "air" then
-                    minetest.set_node(above, { name = "my_mod:plant_stage_1" })
-                    itemstack:take_item()
-                    return itemstack
-                end
-            end
-            return itemstack
-        end
+        inventory_image = def.seed_texture or "seeds_aster.png"
     })
+    --- if core.global_exists("farming") then
+    ---  farming.register_plant(name,
+    ---  {
+    ---     inventory_image =
+    ---  })
+    ---end
 end
 
 -- Node timer callback to handle plant growth
 local function grow_plant(pos, node)
-    for plant_id, def in pairs(Rosa.registered_plants) do
+    for plant_id, def in pairs(Librosa.registered_plants) do
         for stage = 1, #def.textures - 1 do
             if node.name == get_stage_node_name(plant_id, stage) then
                 minetest.set_node(pos, { name = get_stage_node_name(plant_id, stage + 1) })
@@ -143,79 +147,97 @@ local function grow_plant(pos, node)
     end
 end
 
--- Register the plant and its logic
+-- Register a plant as a decoration, and automatically as a crop if any farming mods are installed
 --
 -- Plant Def:
 --
--- {
+--      // Basic Parameters:
+--      name (str)[required]: A name for the plant (this is required for generating the seed / cutting nodes)
+--      description (str)[required]: A simple description
+--      textures (table)[required]: The main image texture, one for each stage of growth
+--      inventory_image (str)[default=texture]: Optionally, provide an inventory image other than the texture specified with texture=""
+--      genus (str)[default="Unknown"]: Optionally, provide a genus to add to the description
+--      species (str)[default="Unknown"]: Optionally, provide a species to add to the description
 --
---      description: (str) A simple description
+--      // Farming/ Growth Parameters:
+--      steps (int)[required]: Number of growth stages to preform until the crop/plant is mature
+--      minlight (float)[default=12]: Minimal light requirements for the crop/plant to be able to grow
+--      maxlight (float)[default=99]: Maximum light level for the crop/plant to be able to grow
+--      can_grow (function)[default=()]: Growth determinate function to provide the default farming mod
 --
---      textures: (table) a list of textures to use for each growth stage, at least two must be provided.
 --
---      growth_time: (num) Maximum amount of time until a growth update can occur
--- }
-function Rosa.register_plant(name, def)
+function Librosa.register_seeding_plant(name, def)
     assert(type(def) == "table", "Plant definition must be a table")
     assert(type(def.textures) == "table" and #def.textures > 1, "Plant must have at least 2 growth stage textures")
     assert(def.growth_time, "growth_time must be specified (in seconds)")
     assert(def.description, "description must be provided")
 
-    Rosa.registered_plants[name] = def
+    Librosa.registered_plants[name] = def
 
-    register_growth_nodes(name, def)
-
-    -- Register planting node (stage 1 only)
-    local initial_node = get_stage_node_name(name, 1)
-    minetest.override_item(initial_node, {
-        on_construct = function(pos)
-            minetest.get_node_timer(pos):start(def.growth_time)
-        end,
-        on_timer = grow_plant
-    })
-
-    -- Add timer function to later stages
-    for stage = 2, #def.textures - 1 do
-        local node_name = get_stage_node_name(name, stage)
-        minetest.override_item(node_name, {
-            on_timer = grow_plant
+    if core.global_exists("farming") then
+        farming.register_plant(name, {
+            description = def.description,
+            steps = def.steps,
+            inventory_image = def.inventory_image or def.texture,
+            minlight = def.minlight or 13,
+            maxlight = def.maxlight or 99,
+            cangrow = def.cangrow or nil
         })
+    elseif core.global_exists("xfarming") then
+        core.debug("(librosa)[ERR] method not implemented")
     end
+    core.debug("(librosa)[WARN] Trying to register " .. name .. " as a seeding plant, but farming is not installed!!")
 end
 
--- Register a plant as a simple decoration, without updating growth stages
+-- Register a plant as a simple decoration
 --
 -- Simple Plant Def:
 -- {
 --      // Basic Parameters:
---      name (str): A name for the plant (this is required for generating the seed / cutting nodes)
---      description (str): A simple description
---      genus (str): Optionally, provide a genus to add to the description
---      species (str): Optionally, provide a species to add to the description
---      texture: The main image texture
+--      name (str)[required]: A name for the plant (this is required for generating the seed / cutting nodes)
+--      description (str)[required]: A simple description
+--      texture (str)[required]: The main image texture
+--      genus (str)[default="Unknown"]: Optionally, provide a genus to add to the description
+--      species (str)[default="Unknown"]: Optionally, provide a species to add to the description
+--      inventory_image (str)[default=texture]: Optionally, provide an inventory image other than the texture specified with texture=""
 --
 --      // Worldgen Parameters:
---      biomes (table): A list of biomes where it's appropriate to place this plant e.g {"default:swamp","othermod:marsh"}
---      surface_nodes (table): A list of surface nodes where it's appropriate to place the plant  e.g { "default:dirt_with_grass,default:dirt_with_snow" }
---      fill_ratio (num):
---      y_min (num): Y minimum range where the plant can spawn
---      y_max (num): Y maximum range where the plant can spawn
---      visual_scale (float): optionally refine the visual size of the plant node.
---      dye_color (str): Optionally, provide a dye color for compat
+--      biomes (table)[required]: A list of biomes where it's appropriate to place this plant e.g {"default:swamp","othermod:marsh"}
+--      surface_nodes (table)[required]: A list of surface nodes where it's appropriate to place the plant  e.g { "default:dirt_with_grass,default:dirt_with_snow" }
+--      y_min (num)[default=1]: Y minimum range where the plant can spawn
+--      y_max (num)[default=31000]: Y maximum range where the plant can spawn
+--      visual_scale (float)[default=1.0]: optionally refine the visual size of the plant node.
 --
 --      // Plant Seed Parameters:
---      seed (true/false): Default false, if set. Plant will have seeds craft items registered.
---      seed_texture: Optional, if no texture is given default fallback will be used.
+--      seed (true/false)[default=false]: If set. Plant will have seeds craft items registered.
+--      seed_texture (str)[default=blank.png]: Optional, if no texture is given default fallback will be used.
+--
+--      // Dye Parameters
+--      dye (true/false)[default=false]: Default false, if set. Plant will have a dye recipe registered.
+--      dye_color (str)[required]: Specify a dye color for the plant to produce when used in the crafting grid
+--      dye_craft_amount (num) [default=1]: Optional, specify how many dye items are crafted per one instance of this plant
+--
+--      // Misc Parameters
+--      pottable (true/false)[default=false]: Optional, specify whether or not the plant should be placeable with the flowerpot mod (if installed)
 --
 -- }
-function Rosa.register_simple_plant(name, def)
+function Librosa.register_simple_plant(name, def)
     assert(type(def) == "table", "Plant definition must be a table")
     assert(def.texture, "no texture!")
     assert(def.name, "A name needs to be given to this plant!")
     assert(def.description, "A basic description is required.")
     assert(def.biomes and type(def.biomes) == "table", "Biomes must be provided as a table.")
 
-    Rosa.registered_plants[name] = def
+    Librosa.registered_plants[name] = def
+
+    local groups = { snappy = 3, flammable = 2, flower = 1, flora = 1 }
+
+    if def.seed == true then
+        minetest.debug("registering seeds for " .. def.name)
+        register_seeds(name, def)
+    elseif def.dye then
+        groups["color_" .. def.dye_color] = 1
+    end
 
     format_description(name, def)
 
@@ -223,16 +245,15 @@ function Rosa.register_simple_plant(name, def)
         description = def.description,
         drawtype = "plantlike",
         tiles = { def.texture },
-        visual_scale = def.visual_scale or 1,
-        inventory_image = def.texture,
+        visual_scale = def.visual_scale or 1.0,
+        inventory_image = def.inventory_image or def.texture,
         wield_image = def.texture,
         waving = 1,
         paramtype = "light",
         sunlight_propagates = true,
         walkable = false,
-        groups = { snappy = 3, flammable = 2, flower = 1, flora = 1 },
+        groups = groups,
         sounds = default.node_sound_leaves_defaults(),
-        dye_color = def.dye_color or "",
         selection_box = {
             type = "fixed",
             fixed = { -0.25, -0.5, -0.25, 0.25, 0.3, 0.25 },
@@ -245,8 +266,8 @@ function Rosa.register_simple_plant(name, def)
         deco_type = "simple",
         species = def.species or "",
         genus = def.genus or "",
-        visual_scale = 0.2,
-        place_on = def.surface_nodes or Rosa.surface_nodes[def.biomes[#def.biomes]],
+        visual_scale = 1.0,
+        place_on = def.surface_nodes or Librosa.surface_nodes[def.biomes[#def.biomes]],
         sidelen = 16,
         waving = true,
         fill_ratio = def.fill_ratio or 0.01,
@@ -255,16 +276,11 @@ function Rosa.register_simple_plant(name, def)
         y_max = def.y_max or 31000,
         decoration = name,
         flags = "force_placement",
-        dye_color = def.dye_color or "",
-        inventory_overlay = function(itemstack)
-            local meta = itemstack:get_meta()
-            local info = meta:get_string("custom_info") or "unknown"
-            return "Owner: " .. info
-        end
     })
 
-    if def.seed then
-        register_seeds(name, def)
+    -- AFTER the node is registered, check def for pottable. Then register with flowerpot
+    if def.pottable then
+        register_pottable_plant(name)
     end
 end
 
@@ -280,10 +296,10 @@ end
 --          growth_rate_max: A maximum amount of time in ticks before the vine can grow
 --          growth_rate_min: A minimum amount of time in ticks before the vine can grow
 --     }
-function Rosa.register_vine(vname, vinedef)
+function Librosa.register_vine(vname, vinedef)
     local stages = vinedef.stages
 
-    Rosa.registered_plants[vname] = def
+    Librosa.registered_plants[vname] = def
     -- Register each stage as a separate node
     for stage = 1, stages do
         minetest.register_node(vname .. "_" .. stage, {
--- init.lua
+++ init.lua
@@ -1,24 +1,25 @@
 local S = minetest.get_translator(minetest.get_current_modname())
 
 -- namespace for flower registration
-Rosa = {}
+Librosa = {}
 
-Rosa.surface_nodes = {}
+Librosa.surface_nodes = {}
 
-dofile(minetest.get_modpath("rosa") .. "/api.lua")
+dofile(minetest.get_modpath("librosa") .. "/api.lua")
 
 -- This is so that modders can simplify the process of adding custom plant generation spreads, even if many different biome altering mods are installed.
 -- The list of surface nodes collected here are used as a default fallback if nothing is provided at plant registration
-function Rosa.extract_surface_nodes()
+function Librosa.extract_surface_nodes()
     for _, biome in pairs(minetest.registered_biomes) do
         if biome and biome.node_top then
             local node_name = biome.node_top
             if node_name and minetest.registered_nodes[node_name] then
-                if not Rosa.surface_nodes[biome.name] then
-                    Rosa.surface_nodes[biome.name] = {}
+                if not Librosa.surface_nodes[biome.name] then
+                    Librosa.surface_nodes[biome.name] = {}
                 end
-                table.insert(Rosa.surface_nodes[biome.name], node_name)
-                minetest.log("action", "[Rosa] Registering surface node from biome: " .. biome.name .. " " .. node_name)
+                table.insert(Librosa.surface_nodes[biome.name], node_name)
+                minetest.log("action",
+                    "[librosa] Registering surface node from biome: " .. biome.name .. " " .. node_name)
             end
         end
     end
@@ -28,5 +29,7 @@ end
 
 minetest.register_on_mods_loaded(function()
     -- get a list of all registered biome surface nodes.
-    Rosa.extract_surface_nodes()
+    Librosa.extract_surface_nodes()
+
+    -- assign dye recipes to all of the plants marked as dye craftable
 end)
